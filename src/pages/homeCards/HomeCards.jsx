a767133239dcd812611be1faccbfa900aef540e1
Toushik <tayebhossain018@gmail.com>
2023-05-04 20:54:28 +0600

    upadted the extra card

--- src/pages/homeCards/HomeCards.jsx
+++ src/pages/homeCards/HomeCards.jsx
@@ -37,7 +37,8 @@ const HomeCards = () => {
                 <div className="bg-gray-100 py-8 container rounded-lg">
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                         <h2 className="text-3xl font-extrabold text-gray-900">Quick, Best, Healthy</h2>
-                        <p className="mt-2 text-lg text-gray-600">30-Minute Healthy and Delicious Indian Recipes: Quick and Easy Meals for Your Busy Lifestyle</p>
+                        <p className="mt-2 text-lg text-gray-600">30-Minute Healthy and Delicious Indian Recipes <span className="text-gray-950">
+                        Quick and Easy Meals for Your Busy Lifestyle</span></p>
                     </div>
                 </div>
 
@@ -50,7 +51,7 @@ const HomeCards = () => {
                     className={`relative bg-white rounded-md shadow-xl m-4 p-6 w-72 sm:w-80 lg:w-96 transform hover:-translate-y-1 hover:shadow-2xl duration-300 ${card.color}`}
                 >
                     <div className="flex items-center mb-6">
-                        <div className="rounded-full bg-white p-2">{card.icon}</div>
+                        <div className="rounded-full bg-white p-2 text-red-700">{card.icon}</div>
                         <h3 className="text-2xl font-bold ml-4">{card.title}</h3>
                     </div>
                     <p className="text-gray-900 mb-6">{card.description}</p>
